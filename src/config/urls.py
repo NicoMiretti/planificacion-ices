578cad75725b5b21b0d8cf40fcf626590acf653d
NicoMiretti <miretti20@gmail.com>
2026-05-12 14:18:25 -0300

    fix: agregar ruta favicon.ico para eliminar warning 404

--- src/config/urls.py
+++ src/config/urls.py
@@ -5,10 +5,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.ico', permanent=True)),
+
     # Apps
     path('', include('apps.core.urls')),
     path('usuarios/', include('apps.usuarios.urls')),
